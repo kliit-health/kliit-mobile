22f3992ba00676e2670a79cb2897e12db928ef7a
Aaron Elkins <akatoll@hotmail.com>
2020-04-29 08:31:42 -0700

    TECH-13: Enable autocorrect on TextInputs (#6)

--- src/components/customInputText/simpleInputText.js
+++ src/components/customInputText/simpleInputText.js
@@ -4,11 +4,7 @@ import React from 'react';
 import { TextInput } from 'react-native';
 
 const InputText = ({ style, ...otherProps }) => (
-    <TextInput
-        {...otherProps}
-        autoCorrect={false}
-        style={style}
-    />
+  <TextInput {...otherProps} style={style} />
 );
 
 export default InputText;
